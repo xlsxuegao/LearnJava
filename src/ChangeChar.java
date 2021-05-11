1b9b5172fdffef12bd330c3dbe475d6f7ac1a8aa
xuls <839046422@qq.com>
2021-05-11 21:53:42 +0800

    转义字符

---
+++ src/ChangeChar.java
@@ -0,0 +1,9 @@
+/**
+ * @author xuls
+ * @date 2021/5/11 21:46
+ */
+public class ChangeChar {
+	public static void main(String[] args) {
+		System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+	}
+}
